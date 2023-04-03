adb203e42380b2c2261701d1e824a857042b3dc0
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-04-03 14:52:14 +0300

    add lualine

--- lua/alexript/packer.lua
+++ lua/alexript/packer.lua
@@ -71,4 +71,8 @@ return require('packer').startup(function(use)
     use { 'fatih/vim-go',
         { run = ':GoUpdateBinares' }
     }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
