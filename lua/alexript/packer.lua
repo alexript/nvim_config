a35e1372505c0030d7c7885596e00d51abe81502
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-04-03 13:25:22 +0300

    add which-key

--- lua/alexript/packer.lua
+++ lua/alexript/packer.lua
@@ -58,6 +58,18 @@ return require('packer').startup(function(use)
             require("nvim-tree").setup {}
         end
     }
-    use{'lewis6991/gitsigns.nvim'
-}
+    use('lewis6991/gitsigns.nvim')
+
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 end)
