2a72b93698c3fc81206a56d022f220a9b82b2051
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-04-03 14:35:44 +0300

    config catppuccin

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,17 @@
 
 require("catppuccin").setup({
     flavour = "macchiato",
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = true,
+        mason = true,
+        treesitter = true,
+        which_key = true,
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
 })
 
 vim.cmd.colorscheme "catppuccin"
