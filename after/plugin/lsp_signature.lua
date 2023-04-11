faa1a13646c94a513ea17d0478fc6944663d3014
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-04-11 15:02:01 +0300

    fix plugins conflict

--- after/plugin/lsp_signature.lua
+++ after/plugin/lsp_signature.lua
@@ -2,5 +2,6 @@ require "lsp_signature".setup({
     bind = true, -- This is mandatory, otherwise border config won't get registered.
     handler_opts = {
         border = "rounded"
-    }
+    },
+    noice = true,
 })
