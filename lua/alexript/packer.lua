bd5c1b879b4eebe2f22cedfb12a6196879b232c6
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-11-21 14:16:52 +0300

    switch telescope to branch 0.1.x

--- lua/alexript/packer.lua
+++ lua/alexript/packer.lua
@@ -17,8 +17,7 @@ return require("packer").startup({
 
         use({
             "nvim-telescope/telescope.nvim",
-            tag = "0.1.1",
-            -- or                            , branch = '0.1.x',
+            branch = '0.1.x',
             requires = { { "nvim-lua/plenary.nvim" } },
             config = function()
                 require("alexript.setup.telescope")
