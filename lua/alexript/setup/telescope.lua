b52a3c9ba2aa071e05cb91ac0f1a918135dcc47c
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-11-22 15:56:48 +0300

    cleanup telescope

--- lua/alexript/setup/telescope.lua
+++ lua/alexript/setup/telescope.lua
@@ -1,8 +1,8 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {}) -- project files
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})       -- git files
-vim.keymap.set("n", "<leader>bb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>bi", builtin.builtin, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>hk", builtin.keymaps, {})
+local ts_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", ts_builtin.find_files, {}) -- project files
+vim.keymap.set("n", "<C-p>", ts_builtin.git_files, {})       -- git files
+vim.keymap.set("n", "<leader>bb", ts_builtin.buffers, {})
+vim.keymap.set("n", "<leader>bi", ts_builtin.builtin, {})
+vim.keymap.set("n", "<leader>fh", ts_builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ps", ts_builtin.live_grep, {})
+vim.keymap.set("n", "<leader>hk", ts_builtin.keymaps, {})
