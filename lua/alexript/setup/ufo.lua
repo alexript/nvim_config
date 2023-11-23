ef5904df609408e2d6a69cfc7fb059a04592f3c8
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-11-23 15:15:15 +0300

    tuneup ufo

--- lua/alexript/setup/ufo.lua
+++ lua/alexript/setup/ufo.lua
@@ -7,17 +7,8 @@ vim.o.foldenable = true
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true,
-}
-local lspconfig = require("lspconfig")
-local language_servers = lspconfig.util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-    lspconfig[ls].setup({
-        capabilities = capabilities,
-        -- you can add other fields for setting up lsp server in this table
-    })
-end
-require("ufo").setup()
+require("ufo").setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+})
