35e2b138d2372b041241db9d48858072516f7120
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-11-24 14:18:09 +0300

    add windows

--- lua/alexript/lazy-plugins/utils.lua
+++ lua/alexript/lazy-plugins/utils.lua
@@ -116,5 +116,18 @@ return {
         config = function()
             require("alexript.setup.blankline")
         end
+    },
+    {
+        "anuvyklack/windows.nvim",
+        dependencies = {
+            "anuvyklack/middleclass",
+            "anuvyklack/animation.nvim"
+        },
+        config = function()
+            vim.o.winwidth = 10
+            vim.o.winminwidth = 10
+            vim.o.equalalways = false
+            require('windows').setup()
+        end
     }
 }
