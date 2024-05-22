dbce4595cc628ac32b9d97abb900a2183e89f080
Alex 'Ript' Malyshev <alexript@gmail.com>
2024-05-22 11:02:25 +0300

    change autohide value

--- lua/alexript/setup/barbar.lua
+++ lua/alexript/setup/barbar.lua
@@ -2,7 +2,7 @@ vim.g.barbar_auto_setup = false
 
 require('barbar').setup({
     animation = true,
-    auto_hide = 2,
+    auto_hide = 1,
     tabpages = true,
     clickable = true,
     icons = {
