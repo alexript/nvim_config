64cf802adc7ba0d9aa3cbdadc13c2c0905b3b095
Alexander Malyshev <a.malyshev@global-system.ru>
2024-12-28 15:55:10 +0300

    gitsign disable word_diff

--- lua/alexript/setup/gitsigns.lua
+++ lua/alexript/setup/gitsigns.lua
@@ -9,8 +9,8 @@ require("gitsigns").setup({
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl = true,      -- Toggle with `:Gitsigns toggle_numhl`
-    linehl = false,     -- Toggle with `:Gitsigns toggle_linehl`
-    word_diff = true,  -- Toggle with `:Gitsigns toggle_word_diff`
+    linehl = true,     -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff = false,  -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir = {
         interval = 1000,
         follow_files = true,
